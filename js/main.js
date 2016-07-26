f9961b587667e661970b3dd2787c12e67f9622e9
nathan anderson <anderson@nathanworking.com>
2016-07-25 21:50:07 -0400

    Working Static Page
    No JS yet - only HTML / CSS setup

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//----------------------------------------------------------------//
+//---------------// Calling Hideseek Live Search //---------------//
+//----------------------------------------------------------------//
+	$('#lightgallery').lightGallery({
+		download: false,
+		counter: false,
+		zoom: false,
+		thumbnail: false,
+		mode: 'lg-fade'
+	});
+
+//----------------------------------------------------------------//
+//---------------// Calling Hideseek Live Search //---------------//
+//----------------------------------------------------------------//
+$(document).ready(function() {
+  $('#search').hideseek({
+		nodata: 'No results found'
+	});
+});
